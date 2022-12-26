efb163c1e25f9b23784106b99c1ad89168985d50
Yalçın <yalcinyalhi@gmail.com>
2022-12-26 21:20:53 +0300

    total people calculation implement

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let saveEl = document.getElementById("save-el");
 let gecmisBilgi = localStorage.getItem("gecmisBilgi");
+let previousEntries = localStorage.getItem("total");
+let totalPeopleEL = document.querySelector("#total-people");
+
 if (gecmisBilgi !== null) {
   saveEl.textContent = localStorage.getItem("gecmisBilgi");
 } else {
   saveEl.textContent = "";
 }
+let peopleArray = [JSON.parse(previousEntries)];
+
+totalPeopleEL.textContent = localStorage.getItem("total");
 
 let countEl = document.getElementById("count-el");
 let count = 0;
@@ -15,9 +21,13 @@ function increment() {
 }
 
 function save() {
-  let countStr = count + " - ";
+  let countStr = `${count} - `;
+  peopleArray.push(count);
   saveEl.textContent += countStr;
   localStorage.setItem("gecmisBilgi", saveEl.textContent);
+  let sum = peopleArray.reduce((a, b) => a + b, 0);
+  localStorage.setItem("total", sum);
+  totalPeopleEL.textContent = localStorage.getItem("total");
   countEl.textContent = 0;
   count = 0;
 }
